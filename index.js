d5c8786e4d00e3db7c3d988c6f6647b121954095
Jamila <jamnjeri@gmail.com>
2022-12-10 15:11:46 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
+function reverseString(word){
+  //create an array from the input string
+  const wordArray = word.split("");
+  //reverse the array
+  const reversedWordArray = wordArray.reverse();
+  //create a string from the reversed array
+  const reversedWord = reversedWordArray.join("");
+  //return the reversed string
+  return reversedWord;
+}
 function isPalindrome(word) {
   // Write your algorithm here
+  //Reverse the input string
+  const reversedWord = reverseString(word);
+  //if the reversed string is the same as the input
+  return word === reversedWord;
 }
 
 /* 
